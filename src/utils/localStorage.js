2aacb197b04f6a4119bed94ce3e3d9c143f808ca
SaleemLawal <slawal1@umbc.edu>
2024-05-30 10:09:30 -0400

    updated add to storage functionalitiy

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -2,21 +2,18 @@ function setLocalStorage() {
     if (!localStorage.getItem('todoList')) {
         localStorage.setItem('todoList', JSON.stringify([]));  
     }
-    // const userData = JSON.parse(localStorage.getItem('todoList'));
-    // console.log(userData);
 }
-
 
 function addToStorage(data) {
     // Get previous data
     const userData = JSON.parse(localStorage.getItem('todoList')) || [];
+    // checks if the new data id already exist before
+    // if it does it replaces it otherwise it makes a new list of todos 
+    const updatedData = userData.some(todo => todo.id === data.id)
+        ? userData.map(todo => (todo.id === data.id ? data : todo))
+        : [...userData, data];
 
-    const arr = [
-        ...userData,
-        data
-    ];
-
-    localStorage.setItem('todoList', JSON.stringify(arr));
+    localStorage.setItem('todoList', JSON.stringify(updatedData));
 }
 
 function getLocalStorageItems(){
@@ -24,10 +21,9 @@ function getLocalStorageItems(){
     return items ? JSON.parse(items) : [];
 }
 
-// eslint-disable-next-line no-unused-vars
-function clearStorage(){
-    localStorage.clear();
-}
+// function clearStorage(){
+//     localStorage.clear();
+// }
 
 function removeItemFromStorage(id){
     // filters the arr and exclude the item with the id to be removed
